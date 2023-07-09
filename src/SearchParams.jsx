550864ae7a0ac7d69a675c00fe1aa7235750925a
Alvaro Capaceta <ingcapadev@gmail.com>
2023-07-08 21:42:44 -0700

    Backup Intro to React Course

---
+++ src/SearchParams.jsx
@@ -0,0 +1,80 @@
+import { useState, useContext } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import { useQuery } from "@tanstack/react-query";
+import useBreedList from "./useBreedList";
+import fetchSearch from "./fetchSearch";
+import Results from "./Results";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [adoptedPet] = useContext(AdoptedPetContext);
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: "",
+  });
+  const [animal, setAnimal] = useState("");
+  const [breeds] = useBreedList(animal);
+
+  const results = useQuery(["search", requestParams], fetchSearch);
+  const pets = results?.data?.pets ?? [];
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          const obj = {
+            animal: formData.get("animal") ?? "",
+            breed: formData.get("breed") ?? "",
+            location: formData.get("location") ?? "",
+          };
+          setRequestParams(obj);
+        }}
+      >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
+
+        <label htmlFor="location">
+          Location
+          <input id="location" name="location" placeholder="Location" />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            name="animal"
+            id="animal"
+            disabled={ANIMALS.length === 0}
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option></option>
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select name="breed" id="breed" disabled={breeds.length === 0}>
+            <option></option>
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
